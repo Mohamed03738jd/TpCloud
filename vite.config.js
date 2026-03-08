88f595fcfbbda9dd246c3123fc346f408b92a2ed
Mohamed03738jd <mohamedelaazzaoui00@gmail.com>
2026-03-08 16:56:02 +0000

    Tp10 1.25

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,21 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname, resolve } from 'path';
+
+// Crée __dirname équivalent en ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
+      '@': resolve(__dirname, 'src'), // exemple d'alias
     },
   },
   test: {
-    globals: true,
-    environment: 'jsdom'
+    globals: true,       // permet d'utiliser test/expect globalement
+    environment: 'jsdom' // nécessaire pour tester React
   }
 });
