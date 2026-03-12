e58900287cd2deeba7fc469a978e0fecf340f3f3
Mohamed03738jd <mohamedelaazzaoui00@gmail.com>
2026-03-12 16:25:20 +0000

    test v0.5

--- test/math.test.jsx
+++ test/math.test.jsx
@@ -2,7 +2,7 @@ import { test, expect } from 'vitest';
 import { add, multiply } from '../src/utils/math';
 
 test('add function adds two numbers', () => {
-  expect(add(2, 3)).toBe(6);
+  expect(add(2, 3)).toBe(5);
   expect(add(-1, 1)).toBe(0);
 });
 
